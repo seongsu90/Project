58c98179c96e66645c9c8c0d9f3acc983a5a8f7e
parkhyuntae <qkrgusxo1993@naver.com>
2016-11-10 18:57:44 +0900

    박현태 이벤트 메뉴리스트

--- JMAppProject/src/main/java/com/mycompany/ourapp/dao/EventDao.java
+++ JMAppProject/src/main/java/com/mycompany/ourapp/dao/EventDao.java
@@ -1,5 +1,53 @@
 package com.mycompany.ourapp.dao;
 
-public class EventDao {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
+import com.mycompany.ourapp.dto.Event;
+
+@Component
+public class EventDao {
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	public int insert(Event event){
+		String sql="insert into event(ename,eresid,esavedfile,einfo,emlname,eprice,estart,eend), values(?,?,?,?,?,?,?,?)";
+		int row = jdbcTemplate.update(
+				sql,
+				event.getEname(),
+				event.geteResid(),
+				event.getEsavedfile(),
+				event.getEinfo(),
+				event.geteMlname(),
+				event.getEprice(),
+				event.getEstart(),
+				event.getEend()
+				);
+		return row;
+	}
+	
+	public int delete(int eResid, String eMlname){
+		String sql="delete from event where eresid=?, emlname=?";
+		int row = jdbcTemplate.update(sql, eResid, eMlname);
+		return row;
+	}
+	
+	public int update(Event event){
+		String sql = "update event set ename=?,eresid=?,esavedfile=?,einfo=?,emlname=?,eprice=?,estart=?,eend=?";
+		int row = jdbcTemplate.update(
+				sql,
+				event.getEname(),
+				event.geteResid(),
+				event.getEsavedfile(),
+				event.getEinfo(),
+				event.geteMlname(),
+				event.getEprice(),
+				event.getEstart(),
+				event.getEend()
+				);
+		return row;
+	}
+	
 }
--- JMAppProject/src/main/java/com/mycompany/ourapp/dao/MenuListDao.java
+++ JMAppProject/src/main/java/com/mycompany/ourapp/dao/MenuListDao.java
@@ -1,5 +1,51 @@
 package com.mycompany.ourapp.dao;
 
-public class MenuListDao {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
+import com.mycompany.ourapp.dto.MenuList;
+
+@Component
+public class MenuListDao {
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	public int insert(MenuList menuList){
+		String sql="insert into menuList(mlname,mlprice,mlresid,mlinfo,mlsavedfile),values(?,?,?,?,?)";
+		int row = jdbcTemplate.update(
+				sql,
+				menuList.getMlname(),
+				menuList.getMlprice(),
+				menuList.getMlResid(),
+				menuList.getMlinfo(),
+				menuList.getMlsavedfile()
+				);
+		return row;
+	}
+	
+	public int modify(MenuList menuList){
+		String sql="update menuList set mlname=?,mlprice=?,mlresid=?,mlinfo=?,mlsavedfile=?";
+		int row = jdbcTemplate.update(
+				sql,
+				menuList.getMlname(),
+				menuList.getMlprice(),
+				menuList.getMlResid(),
+				menuList.getMlinfo(),
+				menuList.getMlsavedfile()
+				);
+		return row;
+	}
+	
+	public int delete(int mlResid, String mlname){
+		String sql="delete from menuList where mlResid=?,mlname=?";
+		int row = jdbcTemplate.update(sql,mlResid,mlname);
+		return row;
+	}
+	
+	public int select(int mlResid, boolean mlishot){
+		String sql="select mlname from menuList where mlResid=?, mlishot=";
+		int row = jdbcTemplate.update(sql,);
+	}
 }
