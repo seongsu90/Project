03616bf0913d10458a2855e930887a4c43548ee3
yooeunji <yooeunji90@naver.com>
2016-11-11 11:28:42 +0900

    유은지 -레스토랑컨트롤라하는중

--- JMAppProject/src/main/java/com/mycompany/ourapp/controller/RestaurantController.java
+++ JMAppProject/src/main/java/com/mycompany/ourapp/controller/RestaurantController.java
@@ -1,5 +1,27 @@
 package com.mycompany.ourapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.mycompany.ourapp.dto.Restaurant;
+import com.mycompany.ourapp.service.RestaurantService;
+
+@Controller
+@RequestMapping("/restaurant")
 public class RestaurantController {
+	@Autowired
+	private RestaurantService restaurantservice;
+	
+	@RequestMapping(value="/add", method=RequestMethod.GET)
+	public String add(Restaurant restaurant){
+		return "restaurant/add";
+	}
+	
+	@RequestMapping(value="/add", method=RequestMethod.POST)
+	public String add("")
+	
+	
 
 }
